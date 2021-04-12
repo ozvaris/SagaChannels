d9d76e11752c575b0429783b6727a204c1208cbe
ozvaris <ozgurozvaris@hotmail.com>
2021-04-12 15:24:08 +0300

    folders_structured

--- index.js
+++ index.js
@@ -4,37 +4,10 @@ import { Provider } from "react-redux";
 import { createStore, applyMiddleware, compose, combineReducers } from "redux";
 import createSagaMiddleware, { delay, eventChannel, END } from "redux-saga";
 import { takeLatest, take, put, call } from "redux-saga/effects";
-import Output from "./output";
-
-import * as actionTypes from "./actionTypes";
-//import { reducer } from "./reducer";
-
-const oneSaga = function* (action) {
-  //console.log("oneSaga - ACTION2");
-  yield call(delay, 500);
-  yield put({ type: actionTypes.ACTION2, payload: action.payload });
-};
-
-const saga = function* () {
-  //console.log("saga - ACTION1");
-  yield takeLatest(actionTypes.ACTION1, oneSaga);
-};
-
-const initialState = {
-  one: 100,
-  two: 100
-};
-
-const reducer = (state = initialState, action) => {
-  switch (action.type) {
-    default:
-      return state;
-    case actionTypes.ACTION1:
-      return { ...state, one: action.payload };
-    case actionTypes.ACTION2:
-      return { ...state, two: action.payload };
-  }
-};
+import Output from "./src/output";
+import * as actionTypes from "./src/actionTypes";
+import reducer from "/src/reducer";
+import { saga } from "./src/saga";
 
 const sagaMiddleware = createSagaMiddleware();
 const composeEnhancers = window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__ || compose;
---
+++ src/saga.js
@@ -0,0 +1,14 @@
+import { delay, eventChannel, END } from "redux-saga";
+import { takeLatest, take, put, call } from "redux-saga/effects";
+import * as actionTypes from "./actionTypes";
+
+const oneSaga = function* (action) {
+  //console.log("oneSaga - ACTION2");
+  yield call(delay, 500);
+  yield put({ type: actionTypes.ACTION2, payload: action.payload });
+};
+
+export const saga = function* () {
+  //console.log("saga - ACTION1");
+  yield takeLatest(actionTypes.ACTION1, oneSaga);
+};
